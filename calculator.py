a3440061413ee90d6b7bcd7759b9f20387c818e4
maslamn <maslamn6420@gmail.com>
2026-04-18 12:29:15 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# Improved Calculator (runs again and again)
+
+while True:
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == "+":
+        print("Result:", num1 + num2)
+
+    elif operator == "-":
+        print("Result:", num1 - num2)
+
+    elif operator == "*":
+        print("Result:", num1 * num2)
+
+    elif operator == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Cannot divide by zero")
+
+    else:
+        print("Invalid operator")
+
+    # Ask user if they want to continue
+    choice = input("Do you want to continue? (yes/no): ").lower()
+    if choice != "yes":
+        print("Calculator stopped.")
+        break
