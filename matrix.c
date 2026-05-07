4009ea0c1584447b58a3530f021282457b39c914
maslamn <maslamn6420@gmail.com>
2026-05-07 15:22:07 +0530

    commit

---
+++ matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+int m,n,A[20][20],B[20][20],C[20][20],i,j;
+printf("Enter matrix size of A and B:");
+scanf("%d%d",&m,&n);
+for(i=0;i<m;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        printf("enter valueof A:");
+        scanf("%d",&A[i][j]);
+    }
+}
+for(i=0;i<m;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        printf("enter value of B:");
+        scanf("%d",&B[i][j]);
+    }
+}
+for(i=0;i<m;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        C[i][j]=A[i][j]+B[i][j];
+        printf("%d\t",C[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
