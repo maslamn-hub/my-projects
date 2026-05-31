72813cd0b641bf3975703a526a56ba10f8054200
maslamn <maslamn6420@gmail.com>
2026-05-31 19:15:45 +0530

    commit scientific calculator

--- python/calculator.py
+++ python/calculator.py
@@ -1,30 +1,61 @@
-# Improved Calculator (runs again and again)
+import math
 
 while True:
-    num1 = float(input("Enter first number: "))
-    operator = input("Enter operator (+, -, *, /): ")
-    num2 = float(input("Enter second number: "))
+    print("\nScientific Calculator")
+    print("+  -  *  /")
+    print("^  (Power)")
+    print("sqrt (Square Root)")
+    print("sin  cos  tan")
+    print("%  (Modulus)")
 
-    if operator == "+":
-        print("Result:", num1 + num2)
+    operator = input("Enter operation: ")
 
-    elif operator == "-":
-        print("Result:", num1 - num2)
+    if operator == "sqrt":
+        num = float(input("Enter number: "))
+        print("Result:", math.sqrt(num))
 
-    elif operator == "*":
-        print("Result:", num1 * num2)
+    elif operator in ["sin", "cos", "tan"]:
+        num = float(input("Enter angle in degrees: "))
 
-    elif operator == "/":
-        if num2 != 0:
-            print("Result:", num1 / num2)
-        else:
-            print("Error: Cannot divide by zero")
+        if operator == "sin":
+            print("Result:", math.sin(math.radians(num)))
+
+        elif operator == "cos":
+            print("Result:", math.cos(math.radians(num)))
+
+        elif operator == "tan":
+            print("Result:", math.tan(math.radians(num)))
 
     else:
-        print("Invalid operator")
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-    # Ask user if they want to continue
+        if operator == "+":
+            print("Result:", num1 + num2)
+
+        elif operator == "-":
+            print("Result:", num1 - num2)
+
+        elif operator == "*":
+            print("Result:", num1 * num2)
+
+        elif operator == "/":
+            if num2 != 0:
+                print("Result:", num1 / num2)
+            else:
+                print("Error: Cannot divide by zero")
+
+        elif operator == "^":
+            print("Result:", num1 ** num2)
+
+        elif operator == "%":
+            print("Result:", num1 % num2)
+
+        else:
+            print("Invalid operator")
+
     choice = input("Do you want to continue? (yes/no): ").lower()
+
     if choice != "yes":
         print("Calculator stopped.")
         break
